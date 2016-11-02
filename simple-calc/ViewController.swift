c0a77382d0dcc3b757b8f3f233d2218c118d588a
kandrewc <andrewk717@gmail.com>
2016-11-01 17:38:07 -0700

    Initial push

---
+++ simple-calc/ViewController.swift
@@ -0,0 +1,228 @@
+//
+//  ViewController.swift
+//  simple-calc
+//
+//  Created by Andrew Kan on 10/23/16.
+//  Copyright © 2016 Andrew Kan. All rights reserved.
+//
+//
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet var calculatorDisplay: UITextField!
+    @IBOutlet weak var rpnSwitchLabel: UILabel!
+    @IBOutlet weak var rpnEnterButton: UIButton!
+    
+    
+    
+    var input : String = ""
+    
+    var inputNumber : Double = 0
+    var result : Double = 0
+    var currentOp : String = ""
+    var operandArray = [Double]()
+    var counting : Bool = false
+    var averaging : Bool = false
+    var factorial : Bool = false
+    
+    var rpnArray = [Double]()
+    
+    var rpnMode: Bool = false;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        currentOp = "="
+        calculatorDisplay.text = ("\(result)")
+        if !rpnMode {
+            rpnEnterButton.isHidden = true
+        } else {
+            rpnEnterButton.isHidden = false
+        }
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func rpnSwitch(_ sender: UISwitch) {
+        if rpnMode {
+            rpnEnterButton.isHidden = true
+        } else {
+            rpnEnterButton.isHidden = false
+        }
+        rpnMode = sender.isOn;
+        print(rpnMode)
+    }
+    
+    @IBAction func buttonInput(_ sender: UIButton) {
+        input += sender.titleLabel!.text!
+        inputNumber = Double(input)!
+        calculatorDisplay.text = ("\(inputNumber)")
+    }
+    
+    @IBAction func rpnEnter(_ sender: UIButton) {
+        rpnArray.append(Double(calculatorDisplay.text!)!)
+        calculatorDisplay.text = ""
+        
+        input = ""
+        
+        print(rpnArray)
+    }
+    
+    @IBAction func operationInput(_ sender: UIButton) {
+        print("operation input!")
+        if rpnMode {
+            if rpnArray.count == 2 {
+                switch sender.titleLabel!.text! {
+                case "+" :
+                    result = rpnArray[0] + rpnArray[1]
+                    calculatorDisplay.text = ("\(result)")
+                case "-" :
+                    result = rpnArray[0] - rpnArray[1]
+                    calculatorDisplay.text = ("\(result)")
+                    
+                case "*" :
+                    result = rpnArray[0] * rpnArray[1]
+                    calculatorDisplay.text = ("\(result)")
+                    
+                case "/" :
+                    result = rpnArray[0] / rpnArray[1]
+                    calculatorDisplay.text = ("\(result)")
+                    
+                case "%" :
+                    result = rpnArray[0].truncatingRemainder(dividingBy: rpnArray[1])
+                    calculatorDisplay.text = ("\(result)")
+                    
+                default:
+                    print("Wrong button")
+                }
+                rpnArray = []
+            }  else {
+                print("Must have two numbers inserted")
+            }
+            
+        } else {
+            input = ""
+            switch currentOp {
+            case "+" :
+                result += inputNumber
+            case "-" :
+                result -= inputNumber
+            case "*" :
+                result *= inputNumber
+            case "/" :
+                result /= inputNumber
+            case "%" :
+                result = result.truncatingRemainder(dividingBy: inputNumber)
+            case "=" :
+                if counting {
+                    result = Double(operandArray.count)
+                } else if averaging {
+                    var sum: Double = 0
+                    for number in operandArray {
+                        sum += Double(number)
+                    }
+                    result = sum / Double(operandArray.count)
+                } else if factorial {
+                    var fact = 1
+                    if operandArray.count == 1 {
+                        for number in 1 ... Int(operandArray[0]) {
+                            fact *= number
+                        }
+                    } else {
+                        print("Can't calculate Factorial")
+                    }
+                    result = Double(fact)
+                } else {
+                    result = inputNumber
+                }
+            default : print("error")
+                
+            }
+            inputNumber = 0
+            calculatorDisplay.text = ("\(result)")
+            if sender.titleLabel!.text == "=" {
+                result = 0
+                inputNumber = 0
+                input = ""
+                operandArray = []
+                counting = false
+                averaging = false
+                factorial = false
+            }
+            currentOp = sender.titleLabel!.text! as String!
+        }
+        
+        let defaults = UserDefaults.standard
+        var tasks = defaults.array(forKey: "tasks")
+        if tasks == nil {
+            tasks = Array()
+        }
+        tasks!.append("HI")
+        defaults.set(tasks, forKey: "tasks")
+
+    }
+    
+    @IBAction func multiOperandInput(_ sender: UIButton) {
+        if rpnMode {
+            input = ""
+            let multiOperand = sender.titleLabel!.text!
+            switch multiOperand {
+            case "Count":
+                calculatorDisplay.text = ("\(rpnArray.count)")
+                rpnArray = []
+                
+            case "Avg" :
+                var sum: Double = 0
+                for number in rpnArray {
+                    sum += Double(number)
+                }
+                result = sum / Double(rpnArray.count)
+                calculatorDisplay.text = ("\(result)")
+                rpnArray = []
+                
+                
+            case "Fact" :
+                if rpnArray.count > 1 {
+                    print("Error")
+                    return
+                } else {
+                    var test: Int = 1
+                    for number in 1 ... Int(rpnArray[0]) {
+                        test *= number
+                    }
+                    calculatorDisplay.text = ("\(test)")
+                    rpnArray = []
+                    
+                }
+            default:
+                print("Error")
+            }
+            
+        } else {
+            input = ""
+            let multiOperand = sender.titleLabel!.text!
+            switch multiOperand {
+            case "Count":
+                counting = true
+            case "Avg" :
+                averaging = true
+            case "Fact" :
+                if operandArray.count > 1 {
+                    print("Error")
+                    return
+                } else {
+                    factorial = true
+                }
+            default:
+                print("Error")
+            }
+            operandArray.append(Double(calculatorDisplay.text!)!)
+            
+        }
+    }
+}
+
